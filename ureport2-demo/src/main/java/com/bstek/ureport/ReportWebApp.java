6f9d76d385d39d6bf4ec0b8ad393cee2335643ea
carcoon <carcoon@163.com>
2020-01-16 10:06:50 +0800

    2.3.2 优化缓存

--- ureport2-demo/src/main/java/com/bstek/ureport/ReportWebApp.java
+++ ureport2-demo/src/main/java/com/bstek/ureport/ReportWebApp.java
@@ -1,25 +1,11 @@
 package com.bstek.ureport;
 
 
-import com.bstek.ureport.config.ClusterConfigurationProperties;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisClusterConnection;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author lqf
